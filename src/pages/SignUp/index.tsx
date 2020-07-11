8424fb0e5f6396b2119cfa2329706a4237e68f88
Guilherme de Jesus Santos <guiljs@gmail.com>
2020-07-11 11:29:34 -0300

    SignUp keyboard usabilty

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import { Image, KeyboardAvoidingView, Platform, View, ScrollView } from 'react-native';
+import { Image, KeyboardAvoidingView, Platform, View, ScrollView, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
@@ -13,6 +13,9 @@ import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 const SignUp: React.FC = () => {
 
     const formRef = useRef<FormHandles>(null);
+
+    const emailInputRef = useRef<TextInput>(null);
+    const passwordInputRef = useRef<TextInput>(null);
 
     const navigation = useNavigation();
     return (
@@ -30,26 +33,55 @@ const SignUp: React.FC = () => {
                         <View>
                             <Title>Crie sua conta</Title>
                         </View>
-                        <Form ref={formRef} onSubmit={(data)=> {
+                        <Form ref={formRef} onSubmit={(data) => {
                             console.log(data);
                         }}>
 
-                            <Input name="name" icon="user" placeholder="Nome" />
-                        <Input name="email" icon="mail" placeholder="E-mail" />
-                        <Input name="password" icon="lock" placeholder="Senha" />
+                            <Input
+                                autoCapitalize="words"
+                                name="name" icon="user" placeholder="Nome"
+                                returnKeyType="next"
+                                onSubmitEditing={() => {
+                                    emailInputRef.current?.focus();
+                                }}
+                            />
 
-                        <Button onPress={() => {
-                            formRef.current?.submitForm();
-                        }} >Entrar
+
+                            <Input
+                                ref={emailInputRef}
+                                keyboardType="email-address"
+                                autoCorrect={false}
+                                autoCapitalize="none"
+                                name="email" icon="mail" placeholder="E-mail"
+                                returnKeyType="next"
+                                onSubmitEditing={() => {
+                                    passwordInputRef.current?.focus();
+                                }}
+                            />
+
+                            <Input
+                                ref={passwordInputRef}
+                                secureTextEntry
+                                name="password" icon="lock" placeholder="Senha"
+                                textContentType="newPassword"
+                                returnKeyType="send"
+                                onSubmitEditing={() => {
+                                    formRef.current?.submitForm();
+                                }}
+                            />
+
+                            <Button onPress={() => {
+                                formRef.current?.submitForm();
+                            }} >Entrar
                             </Button>
                         </Form>
                     </Container>
                 </ScrollView>
-        </KeyboardAvoidingView>
-        <BackToSignIn onPress={() => navigation.goBack()}>
-            <Icon name="arrow-left" size={20} color="#fff" />
-            <BackToSignInText>Voltar para logon</BackToSignInText>
-        </BackToSignIn>
+            </KeyboardAvoidingView>
+            <BackToSignIn onPress={() => navigation.goBack()}>
+                <Icon name="arrow-left" size={20} color="#fff" />
+                <BackToSignInText>Voltar para logon</BackToSignInText>
+            </BackToSignIn>
         </>
     );
 };
