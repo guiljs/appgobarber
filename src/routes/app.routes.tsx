1783cdc7a0a08f86661f7fbae23524c6396bceac
Guilherme de Jesus Santos <guiljs@gmail.com>
2020-10-20 23:23:44 -0300

    Creating routes for Profile and Appointments

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import DashBoard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+import CreateAppointment from '../pages/CreateAppointment';
+import AppointmentCreated from '../pages/AppointmentCreated';
 
 const AppAuth = createStackNavigator();
 
@@ -13,6 +16,10 @@ const AppAuthRoutes: React.FC = () => (
     }}
     >
         <AppAuth.Screen name="Dashboard" component={DashBoard} />
+        <AppAuth.Screen name="CreateAppointment" component={CreateAppointment} />
+        <AppAuth.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+        <AppAuth.Screen name="Profile" component={Profile} />
     </AppAuth.Navigator>
 );
 
