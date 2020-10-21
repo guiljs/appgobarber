15a307e3b0d0f8bf5326c16e43cf448056d11fc7
Guilherme de Jesus Santos <guiljs@gmail.com>
2020-10-20 23:59:28 -0300

    Header with avatar to profile

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -2,9 +2,16 @@ import React, { createContext, useCallback, useState, useContext, useEffect } fr
 import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  avatar_url: string;
+  name: string;
+  email: string;
+}
+
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SignInCredentials {
@@ -12,7 +19,7 @@ interface SignInCredentials {
   password: string;
 }
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
   loading: boolean;
